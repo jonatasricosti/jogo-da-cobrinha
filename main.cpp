77592c88cb7896dcb4e79ab830a9b457de88e7ea
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-01-11 21:22:01 -0300

    framerate adicionado

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SDL.h>
 
+
 SDL_Event evento;
 SDL_Surface *tela = NULL;
 bool executando = true;
@@ -7,6 +8,11 @@ bool executando = true;
 const int screen_width = 300;
 const int screen_height = 400;
 const int screen_bpp = 32;
+
+// para o framerate
+Uint32 start = 0;
+const int fps = 16;
+const int framerate =  1000/fps;
 
 int main(int argc, char*args[])
 {
@@ -17,17 +23,22 @@ tela = SDL_SetVideoMode(screen_width,screen_height,screen_bpp,SDL_SWSURFACE);
 // game loop
 while(executando)
 {
+    start = SDL_GetTicks();
     while(SDL_PollEvent(&evento))
     {
-        // clicou no x
+        // clicou no x da janela
         if(evento.type == SDL_QUIT)
         {
-            executando = false;
+            executando = false; // fecha o programa
         }
     }
 
     SDL_FillRect(tela, 0, 0xffffff);
     SDL_Flip(tela);
+    if(framerate > (SDL_GetTicks()-start))
+    {
+        SDL_Delay(framerate - (SDL_GetTicks()-start));
+    }
 }
 
 SDL_Quit();

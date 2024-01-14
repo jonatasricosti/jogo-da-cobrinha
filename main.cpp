b4f424018c1441fae1fa081e61242704b1dde67b
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-01-13 22:21:56 -0300

    adicionado uma função que carrega uma imagem bmp e deixa o fundo transparente

--- main.cpp
+++ main.cpp
@@ -14,6 +14,31 @@ Uint32 start = 0;
 const int fps = 16;
 const int framerate =  1000/fps;
 
+// use essa função pra carregar uma imagem.bmp e deixa o fundo transparente
+SDL_Surface *fundo_transparente(const char *filename, Uint8 red, Uint8 green, Uint8 blue)
+{
+    SDL_Surface *load = NULL;
+    SDL_Surface *otimizado = NULL;
+
+    load = SDL_LoadBMP(filename);
+
+    if(load != NULL)
+    {
+        otimizado = SDL_DisplayFormat(load);
+        SDL_FreeSurface(load);
+
+        if(otimizado != NULL)
+        {
+            SDL_SetColorKey(otimizado, SDL_SRCCOLORKEY, SDL_MapRGB(otimizado->format, red, green, blue));
+        }
+    }
+
+    return otimizado;
+}
+
+
+
+
 // use essa função pra desenhar uma imagem na tela
 void DrawImage(int x, int y, SDL_Surface *image)
 {
@@ -25,12 +50,14 @@ void DrawImage(int x, int y, SDL_Surface *image)
 }
 
 SDL_Surface *tutorialImage = NULL;
+SDL_Surface *pauseImage = NULL;
 
 // use essa função pra carregar arquivos
 // nota: essa função só deve ser chamada no começo do programa
 void LoadFiles()
 {
     tutorialImage = SDL_LoadBMP("tutorial.bmp");
+    pauseImage = fundo_transparente("pause.bmp", 0,255,255);
 }
 
 
@@ -39,6 +66,7 @@ void LoadFiles()
 void CloseFiles()
 {
     SDL_FreeSurface(tutorialImage);
+    SDL_FreeSurface(pauseImage);
 }
 
 int main(int argc, char*args[])
@@ -61,7 +89,9 @@ while(executando)
         }
     }
 
-    DrawImage(0,0,tutorialImage);
+    //DrawImage(0,0,tutorialImage);
+
+    DrawImage(0,0,pauseImage);
 
     SDL_Flip(tela);
     if(framerate > (SDL_GetTicks()-start))

05d2dc55d28ca94b4098274103981f672cce905b
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-01-14 22:56:10 -0300

    adicionado a função que desenha texto na tela

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ SDL_Event evento;
 SDL_Surface *tela = NULL;
 bool executando = true;
 
-const int screen_width = 300;
-const int screen_height = 400;
+const int screen_width = 640;
+const int screen_height = 480;
 const int screen_bpp = 32;
 
 // para o framerate
@@ -14,7 +14,7 @@ Uint32 start = 0;
 const int fps = 16;
 const int framerate =  1000/fps;
 
-// use essa função pra carregar uma imagem.bmp e deixa o fundo transparente
+// use essa função pra carregar uma imagem.bmp e deixar o fundo transparente
 SDL_Surface *fundo_transparente(const char *filename, Uint8 red, Uint8 green, Uint8 blue)
 {
     SDL_Surface *load = NULL;
@@ -49,8 +49,36 @@ void DrawImage(int x, int y, SDL_Surface *image)
     SDL_BlitSurface(image, NULL, tela, &mover);
 }
 
+// use essa função pra desenhar uma cortada na tela
+void DrawImageFrame(int x, int y, SDL_Surface *source, SDL_Surface *destination, int width, int height, int frame)
+{
+    SDL_Rect mover;
+    mover.x = x;
+    mover.y = y;
+
+    int coluna = source->w / width;
+
+    SDL_Rect corte;
+    corte.x = (frame % coluna)*width;
+    corte.y = (frame / coluna)*height;
+    corte.w = width;
+    corte.h = height;
+
+    SDL_BlitSurface(source, &corte, destination, &mover);
+}
+
+// use essa função pra desenhar texto na tela
+void DrawText(int x, int y, SDL_Surface *source, SDL_Surface *destination, char texto[], int charSize, int start)
+{
+	for(unsigned int i = 0; i < strlen(texto); i++)
+    {
+        DrawImageFrame(x+i*charSize, y, source, destination, charSize, charSize, texto[i]-start);
+    }
+}
+
 SDL_Surface *tutorialImage = NULL;
 SDL_Surface *pauseImage = NULL;
+SDL_Surface *whitefontImage = NULL;
 
 // use essa função pra carregar arquivos
 // nota: essa função só deve ser chamada no começo do programa
@@ -58,6 +86,7 @@ void LoadFiles()
 {
     tutorialImage = SDL_LoadBMP("tutorial.bmp");
     pauseImage = fundo_transparente("pause.bmp", 0,255,255);
+    whitefontImage = fundo_transparente("fontes/whitefont.bmp", 255,0,255);
 }
 
 
@@ -67,6 +96,7 @@ void CloseFiles()
 {
     SDL_FreeSurface(tutorialImage);
     SDL_FreeSurface(pauseImage);
+    SDL_FreeSurface(whitefontImage);
 }
 
 int main(int argc, char*args[])
@@ -89,9 +119,7 @@ while(executando)
         }
     }
 
-    //DrawImage(0,0,tutorialImage);
-
-    DrawImage(0,0,pauseImage);
+    DrawText(0,0,whitefontImage,tela,"Eu gosto muito de programar c++",16,32);
 
     SDL_Flip(tela);
     if(framerate > (SDL_GetTicks()-start))
